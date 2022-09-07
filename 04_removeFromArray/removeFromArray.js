77112514dcc4fdb521ca3f6f5939c47b129f9440
AnthonyPantaleo <anthpantaleo@gmail.com>
2022-09-06 22:41:43 -0400

    finished 2 more exercises. sumAll was annoying

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...args) {
+  let changedArray = arr;
+  for (let i = 0; i < arguments.length; i++) {
+    let index = args[i];
+    let checkedValue = changedArray.indexOf(index);
+    if (checkedValue != -1) {
+      changedArray.splice(checkedValue, 1);
+    } else {
+      // do nothing
+    }
+  }
+  return changedArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let sum = 0;
+  let num1 = a;
+  let num2 = b;
+  if (
+    a < b &&
+    a > -1 &&
+    b > -1 &&
+    typeof a === "number" &&
+    typeof b === "number"
+  ) {
+    for (let i = num1; i <= num2; i++) {
+      sum += i;
+    }
+  } else if (
+    b < a &&
+    a > -1 &&
+    b > -1 &&
+    typeof a === "number" &&
+    typeof b === "number"
+  ) {
+    for (let i = num2; i <= num1; i++) {
+      sum += i;
+    }
+  } else {
+    return "ERROR";
+  }
 
+  return sum;
 };
 
 // Do not edit below this line
