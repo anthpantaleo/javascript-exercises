a4e14ae11c1637ebd7ae5110934d64d7e20061f8
AnthonyPantaleo <anthpantaleo@gmail.com>
2022-09-06 23:06:23 -0400

    Finished all 7 from Part 4 Odin. Not too bad, once you get grooving. Looking at tests seems to help a lot!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function (num) {
+  let farenheiit = num;
+  let toCelcius = Math.round((farenheiit - 32) * (5 / 9) * 10) / 10;
+  return toCelcius;
 };
 
-const ctof = function() {
-
+const ctof = function (num) {
+  let celcius = num;
+  let toFarenheit = Math.round((celcius * (9 / 5) + 32) * 10) / 10;
+  return toFarenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
