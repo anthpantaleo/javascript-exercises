584adaf84175dc320de44961137b35d00af2924e
AnthonyPantaleo <anthpantaleo@gmail.com>
2022-09-06 22:57:11 -0400

    leapYear complete. Pretty easy, just some ifs and elifs and elses

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  let isLeapYear = false;
+  if (year % 100 == 0) {
+    if (year % 400 == 0) {
+      isLeapYear = true;
+    } else {
+      isLeapYear = false;
+    }
+  } else if (year % 4 == 0) {
+    isLeapYear = true;
+  } else {
+    isLeapYear = false;
+  }
+  return isLeapYear;
 };
 
 // Do not edit below this line
